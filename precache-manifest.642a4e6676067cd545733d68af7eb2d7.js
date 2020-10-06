db9a524f38fae2fa548cd979cfa83f09c86f404f
Akash Kumar Pal <akashpal442@gmail.com>
2020-10-07 01:24:50 +0530

    Updates

---
+++ precache-manifest.642a4e6676067cd545733d68af7eb2d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e594f10f9070440eb7f6b38c5a54c93",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "5b0743be364f9d5b6c0a",
+    "url": "/react-todo/static/css/main.ff87a77a.chunk.css"
+  },
+  {
+    "revision": "89a9c4e07ab6e20dd50a",
+    "url": "/react-todo/static/js/2.3d0faa5e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo/static/js/2.3d0faa5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b0743be364f9d5b6c0a",
+    "url": "/react-todo/static/js/main.18a3fead.chunk.js"
+  },
+  {
+    "revision": "c62e486fc828da99e4ee",
+    "url": "/react-todo/static/js/runtime-main.7b490efb.js"
+  }
+]);
